22a989dba92cd71f6a6eedfa4dd2aaac91f1241f
Evens Pompe <evenspompe9593@gmail.com>
2022-07-01 10:25:40 +0200

    feat(main): add loginModal and ipcMain event "close-app"

--- main.js
+++ main.js
@@ -1,36 +1,44 @@
-const path = require('path');
-const { app, BrowserWindow } = require('electron');
+const path = require("path");
+const { app, BrowserWindow, ipcMain } = require("electron");
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 1000,
-        height: 800,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true
-        },
-    })
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
+  });
 
-    win.loadFile('index.html')
+  win.loadFile("index.html");
+  const loginModal = new BrowserWindow({
+    parent: win,
+    modal: true,
+    show: true,
+    width: 500,
+    height: 250,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, "login-preload.js"),
+      nodeIntegration: true,
+    },
+  });
 
-    const win2 = new BrowserWindow({
-        width: 1000,
-        height: 800,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
-            nodeIntegration: true
-        },
-    })
-
-    win2.loadFile('index.html')
+  loginModal.loadFile("login.html");
 }
 
 app.whenReady().then(() => {
-    createWindow()
-    app.on('activate', function () {
-        if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+  createWindow();
+  app.on("activate", async () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      const win = await createWindow();
+      ipcMain.on("close-app", () => {
+        win.hide();
+      });
+    }
+  });
+});
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-})
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
