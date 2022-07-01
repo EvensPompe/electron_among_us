de22fb92c78741a4ac4b5b09c880b0b485873022
Evens Pompe <evenspompe9593@gmail.com>
2022-07-01 10:16:36 +0200

    fix(preload): fix render player bug

--- preload.js
+++ preload.js
@@ -1,61 +1,70 @@
-const io = require('socket.io-client');
-const AnimationFrame = require('./client/classes/AnimationFrame');
-const GameImage = require('./client/classes/GameImage');
-const GamePlayer = require('./client/classes/GamePlayer');
-const KeyBoardPlayer = require('./client/classes/KeyBoardPlayer');
+const { io } = require("socket.io-client");
+const AnimationFrame = require("./client/classes/AnimationFrame");
+const GameImage = require("./client/classes/GameImage");
+const GamePlayer = require("./client/classes/GamePlayer");
+const KeyBoardPlayer = require("./client/classes/KeyBoardPlayer");
 const socket = io(`http://localhost:${3002}`);
+
 const gameImageData = [
-    {
-        role: "background",
-        src: "./assets/PC Computer - Among Us - Dropship Lobby.png",
-        alt: "ship background",
-        xSource: 10,
-        ySource: 15,
-        sourceWidth: 1220,
-        sourceHeight: 980,
-        xDest: 200,
-        yDest: 150,
-        destWidth: 1220 / 2,
-        destHeight: 980 / 2
-    }
+  {
+    role: "background",
+    src: "./assets/PC Computer - Among Us - Dropship Lobby.png",
+    alt: "ship background",
+    xSource: 10,
+    ySource: 15,
+    sourceWidth: 1220,
+    sourceHeight: 980,
+    xDest: 200,
+    yDest: 150,
+    destWidth: 1220 / 2,
+    destHeight: 980 / 2,
+  },
 ];
 
-socket.on("newPlayer", (data) => {
-    const newPlayerData = {
-        ...data,
-        role: "player",
-        src: "./assets/Base/idle.png",
-        alt: "player images",
-        xSource: 10,
-        ySource: 10,
-        sourceWidth: 165,
-        sourceHeight: 211,
-        xDest: 400,
-        yDest: 400,
-        destWidth: 165 / 3.5,
-        destHeight: 211 / 3.5
-    };
-    gameImageData.push(newPlayerData);
-    console.log(gameImageData);
+const drawGame = () => {
+  const mainCanvas = document.getElementById("mainCanvas");
+  const ctx = mainCanvas.getContext("2d");
+  const gameImages = [];
+  gameImageData.forEach(({ role, src, alt, name, id, ...imageData }) => {
+    const image = new Image();
+    image.src = src;
+    image.alt = alt;
+    if (role === "player") {
+      gameImages.push({
+        gamePlayer: new GamePlayer(
+          name,
+          id,
+          image,
+          ...Object.values(imageData)
+        ),
+      });
+    } else {
+      gameImages.push({
+        gameImage: new GameImage(image, ...Object.values(imageData)),
+      });
+    }
+  });
+
+  const keyBoardPlayer = new KeyBoardPlayer();
+  new AnimationFrame().refreshFrame(gameImages, ctx, keyBoardPlayer);
+};
+
+socket.on("server:player:new", (data) => {
+  gameImageData.push({
+    ...data,
+    role: "player",
+    src: "./assets/Base/idle.png",
+    alt: "player images",
+    xSource: 10,
+    ySource: 10,
+    sourceWidth: 165,
+    sourceHeight: 211,
+    xDest: 400,
+    yDest: 400,
+    destWidth: 165 / 3.5,
+    destHeight: 211 / 3.5,
+  });
+  drawGame();
 });
 
-window.addEventListener('DOMContentLoaded', () => {
-    const mainCanvas = document.getElementById('mainCanvas');
-    const ctx = mainCanvas.getContext('2d');
-    const gameImages = [];
-
-    gameImageData.forEach(({ role, src, alt, name, id, ...imageData }) => {
-        const image = new Image();
-        image.src = src;
-        image.alt = alt;
-        if (role === "player") {
-            gameImages.push({ gamePlayer: new GamePlayer(name, id, image, ...Object.values(imageData)) });
-        } else {
-            gameImages.push({ gameImage: new GameImage(image, ...Object.values(imageData)) });
-        }
-    });
-
-
-    const keyBoardPlayer = new KeyBoardPlayer();
-    new AnimationFrame().refreshFrame(gameImages, ctx, keyBoardPlayer);
-});
+window.addEventListener("DOMContentLoaded", drawGame);
